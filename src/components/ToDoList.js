721d517823a60a9866738bff429c3d65e366f369
Cale Riggs <cale03@gmail.com>
2019-09-25 23:28:26 -0500

    Converted to Hooks
    Done for the final result of the class.

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -11,38 +11,25 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import DeleteIcon from '@material-ui/icons/Clear';
 
+function ToDoList() {
 
-export default class ToDoList extends Component {
-  constructor(props) {
-    super(props);
+  const [allItems, setAllItems] = useState([]);
+  const [checked, setChecked] = useState([]);
 
-    this.state = {
-      allItems: [],
-      checked: [],
-    }
-
-    this.handleToggleCheckbox = this.handleToggleCheckbox.bind(this);
-    this.editItemText = this.editItemText.bind(this);
-    this.addItem = this.addItem.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-  }
-
-  componentDidMount() {
-    document.title = 'Get It Done!'
-  }
-
-  componentDidUpdate() {
-    const { allItems, checked } = this.state;
+  useEffect(() => {
+    console.info('updating remaining tasks')
     const remainingNumOfTasks = allItems.length - checked.length
     document.title = `${remainingNumOfTasks} remaining tasks`;
-  }
+  }, [allItems.length, checked.length])
 
-  handleToggleCheckbox(itemIndex) {
-    const { checked } = this.state;
+  useEffect(() => {
+    console.info('Loaded Page');
+    document.title = 'Get It Done!'
+  }, [])
+
+  const handleToggleCheckbox = (itemIndex) => {
     const currentIndex = checked.indexOf(itemIndex);
     const newChecked = [...checked];
-
-    console.log({ itemIndex, checked, currentIndex, newChecked });
 
     if (currentIndex === -1) {
       newChecked.push(itemIndex)
@@ -50,94 +37,79 @@ export default class ToDoList extends Component {
       newChecked.splice(currentIndex, 1);
     }
 
-    this.setState({
-      checked: newChecked,
-    })
+    setChecked(newChecked);
   }
 
-  editItemText = (event, index) => {
-    const { allItems } = this.state;
+  const editItemText = (event, index) => {
     const { target } = event;
     const { value } = target;
     const newItems = [...allItems]
 
     newItems[index] = value;
 
-    this.setState({
-      allItems: newItems
-    })
+    setAllItems(newItems)
   }
 
-  addItem() {
-    const { allItems } = this.state;
+  const addItem = () => {
     const newItems = [...allItems];
     newItems.push('')
 
-    console.log({ allItems, newItems });
-
-    return this.setState({
-      allItems: newItems
-    }, () => console.log('state', this.state))
+    setAllItems(newItems)
   }
 
-  deleteItem(index) {
-    const { allItems } = this.state;
+  const deleteItem = (index) => {
     const updatedItems = [...allItems];
 
     updatedItems.splice(index, 1)
 
-    this.setState({
-      allItems: updatedItems
-    })
+    setAllItems(updatedItems)
   }
 
-  render() {
-    const { allItems, checked } = this.state;
-    console.log('state', this.state)
-    return (
-      <>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6">To Do List</Typography>
-          </Toolbar>
-        </AppBar>
-        <List>
-          {
-            allItems.map((item, index) =>
-              <ListItem>
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={checked.indexOf(index) !== -1}
-                    onClick={() => this.handleToggleCheckbox(index)}
-                  />
-                </ListItemIcon>
-                <TextField
-                  fullWidth
-                  value={item}
-                  onChange={e => this.editItemText(e, index)}
-                  placeholder="Enter new task..."
-                  InputProps={{
-                    endAdornment: (
-                      <IconButton
-                        style={{ padding: 3 }}
-                        onClick={() => this.deleteItem(index)}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    )
-                  }}
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">To Do List</Typography>
+        </Toolbar>
+      </AppBar>
+      <List>
+        {
+          allItems.map((item, index) =>
+            <ListItem>
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={checked.indexOf(index) !== -1}
+                  onClick={() => handleToggleCheckbox(index)}
                 />
-              </ListItem>
-            )
-          }
-          <ListItem>
-            <Button onClick={this.addItem} variant="contained">
-              Add Item
+              </ListItemIcon>
+              <TextField
+                fullWidth
+                value={item}
+                onChange={e => editItemText(e, index)}
+                placeholder="Enter new task..."
+                InputProps={{
+                  endAdornment: (
+                    <IconButton
+                      style={{ padding: 3 }}
+                      onClick={() => deleteItem(index)}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  )
+                }}
+              />
+            </ListItem>
+          )
+        }
+        <ListItem>
+          <Button onClick={addItem} variant="contained">
+            Add Item
           </Button>
-          </ListItem>
-        </List>
-      </>
-    )
-  }
+        </ListItem>
+      </List>
+    </>
+  )
 }
+
+export default ToDoList
